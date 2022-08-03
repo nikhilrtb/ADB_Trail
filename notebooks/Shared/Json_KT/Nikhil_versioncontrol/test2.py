9d640d65fdd2921c3ea74f2f2dbdc7479883e718
nikhilrtb <110535222+nikhilrtb@users.noreply.github.com>
2022-08-03 17:35:05 +0530

    rebase from main to nikhil-test

--- notebooks/Shared/Json_KT/Nikhil_versioncontrol/test2.py
+++ notebooks/Shared/Json_KT/Nikhil_versioncontrol/test2.py
@@ -1,3 +1,9 @@
 # Databricks notebook source
 print("Nikhil")
 print("Added Pradeep to the chat")
+print("new commits")
+
+# COMMAND ----------
+
+print("after merge now doing new changes only in pradeep's branch")
+print("added manually from git edit")
