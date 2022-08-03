9b2e115d3b7d411759c48ea6fcdac046f4985750
nikhilrtb <110535222+nikhilrtb@users.noreply.github.com>
2022-08-03 17:22:10 +0530

    pradeep's branch only

--- notebooks/Shared/Json_KT/Nikhil_versioncontrol/test2.py
+++ notebooks/Shared/Json_KT/Nikhil_versioncontrol/test2.py
@@ -1,3 +1,8 @@
 # Databricks notebook source
 print("Nikhil")
 print("Added Pradeep to the chat")
+print("new commits")
+
+# COMMAND ----------
+
+print("after merge now doing new changes only in pradeep's branch")
